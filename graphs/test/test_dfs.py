df6242c1a2d0875cb5b2bbb9f0d66d677e41cd04
Holger Thies <holger.thies@gmx.de>
2014-05-22 20:25:46 +0200

    Added DFS

---
+++ graphs/test/test_dfs.py
@@ -0,0 +1,30 @@
+import os
+import sys
+sys.path.append('..')
+import unittest
+import graph
+import dfs
+class DfsTests(unittest.TestCase):
+	def testBfs(self):
+		G = graph.Graph()
+		G.addEdge(1,2)
+		G.addEdge(2,3)
+		G.addEdge(4,1)
+		G.addEdge(5,1)
+		G.addEdge(6,1)
+		G.addEdge(7,6)
+		G.addEdge(3,1)
+		G.addEdge(8,7)
+		G.addEdge(9,8)
+		G.addEdge(2,8)
+		G.addEdge(9,10)
+		processingOrder = []
+		processFun = lambda v: processingOrder.append(v)
+		dfs.dfs(G, 1, processFun, lambda v : v)
+		self.failUnlessEqual(processingOrder, [1,2,8,9,10,7,6,3,4,5])
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+    main()
